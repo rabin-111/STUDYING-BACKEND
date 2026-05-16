9be57417594eb50f28816ce036016ff17e9a554d
RABIN POUDEL <131688762+rabin-111@users.noreply.github.com>
2026-05-16 09:50:20 +0545

    Delete i.js

--- i.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const Here = fs.readFileSync('./final/txt/input.txt' );
- console.log(Here);
-const wr =`HERa maile janeko yeti ho ${Here}.\n lekheko yo date ma ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt' , wr);
